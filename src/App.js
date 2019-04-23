4f5f4af7edfef547090dd42adacadaabc8e1ce1b
Tal Zeev <zeevtorrestal@gmail.com>
2019-04-23 15:21:11 +0900

    pretty code app.js

--- src/App.js
+++ src/App.js
@@ -29,8 +29,7 @@ class App extends Component {
       return (
         fetch('http://localhost:3000/locations'+url)
         .then(response => response.json())
-      )
-    }
+      )}
     
     this.showDestination = (originSelected) => {
       this.setState({
@@ -43,87 +42,75 @@ class App extends Component {
     this.showDates = (destinationSelected) => {
       this.setState({
         destinationSelected,
-      })
-    }
+      })}
 
     this.showSubmitOneWay = (dateDepartSelected) => {
       this.setState({
         dateDepartSelected,
-      })
-    }
+      })}
 
     this.showSubmitReturn = (dateReturnSelected) => {
       this.setState({
         dateReturnSelected,
-      })
-    }
-
+      })}
 
     this.showDateReturn = () => {
       this.setState({
         checked: !this.state.checked
       })
-      //(this.state.checked ?  )
-    }
-  
-  
+    }}
+
+
+  componentDidMount() {
+    this.getLocations('')
+    .then(origin => this.setState({ origin }))
   }
-
-
-
-
-    componentDidMount() {
-      this.getLocations('')
-      .then(origin => this.setState({ origin }))
-    }
-
-
-    render() {
-      return (
-        <div className="App-header">
-        <div className="top"></div>
-        <Segment style= {{
-          backgroundColor:'rgba(102, 102, 102, 0.5)',
-          position: 'fixed',
-          width: '100vh',
-          left: '50vh'
-        }}
-        >
-        <div>
-        <DDOrigin 
-          showDestination = {this.showDestination} 
-          state = {this.state} 
+  render() {
+    return (
+      <div className="App-header">
+      <div className="top"></div>
+      <Segment style= {{
+        backgroundColor:'rgba(102, 102, 102, 0.5)',
+        position: 'fixed',
+        width: '100vh',
+        left: '50vh'
+      }}
+      >
+      <div>
+      <DDOrigin 
+        showDestination = {this.showDestination} 
+        state = {this.state} 
+      />
+      </div>
+      <div style={{ display: this.state.originSelected ? 'block' : 'none' }}>
+      <DDDestination  
+        showDates = {this.showDates}
+        state = {this.state} 
+      />
+      </div>
+      <div style={{ display: this.state.destinationSelected ? 'block' : 'none' }}>
+        <DateOrigin
+          showSubmit = {this.showSubmitOneWay}
+          showDateReturn = {this.showDateReturn}
+          state = {this.state}
         />
-        </div>
-        <div style={{ display: this.state.originSelected ? 'block' : 'none' }}>
-        <DDDestination  
-          showDates = {this.showDates}
-          state = {this.state} 
+      </div>
+      <div style={{ display: this.state.checked ? 'block' : 'none' }}>
+        <DateReturn
+          showSubmit = {this.showSubmitReturn}
+          state = {this.state}
         />
-        </div>
-        <div style={{ display: this.state.destinationSelected ? 'block' : 'none' }}>
-          <DateOrigin
-            showSubmit = {this.showSubmitOneWay}
-            showDateReturn = {this.showDateReturn}
-            state = {this.state}
-          />
-        </div>
-        <div style={{ display: this.state.checked ? 'block' : 'none' }}>
-          <DateReturn
-            showSubmit = {this.showSubmitReturn}
-            state = {this.state}
-          />
-        </div>
-        <div style={{
-          display: (this.state.dateDepartSelected && !this.state.checked) || (this.state.dateReturnSelected && this.state.checked) ? 'block' : 'none' 
-          }}>
-          <SubmitButton
-          />
-        </div>
-        </Segment>
-        </div>
-        )
-    }
+      </div>
+      <div style={{
+        display: (this.state.dateDepartSelected && !this.state.checked) || (this.state.dateReturnSelected && this.state.checked) ? 'block' : 'none' 
+        }}>
+        <SubmitButton
+        />
+      </div>
+      </Segment>
+      </div>
+      )
   }
+}
 
 export default App; 
